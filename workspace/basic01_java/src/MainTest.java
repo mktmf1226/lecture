5907f18bca66f878a992ab348f5ed3eea3f1bfb7
Daseul <mktmf1226@gmail.com>
2022-09-07 18:08:50 +0900

    220907 update

---
+++ workspace/basic01_java/src/MainTest.java
@@ -0,0 +1,18 @@
+
+public class MainTest {
+
+	public static void main(String[] args) {//argument string 약자
+		//oop0907.Test06_main.java 참조
+
+		for(int i=0; i<args.length; i++) {
+			System.out.printf(args[i]);
+		}//for end
+		
+		
+		
+		
+		
+		
+		
+	}//main() end
+}//class end
--- workspace/basic01_java/src/oop0905/Test06_quiz.java
+++ workspace/basic01_java/src/oop0905/Test06_quiz.java
@@ -40,8 +40,5 @@ public class Test06_quiz {
 		
 		
 	}//main() end
-<<<<<<< HEAD
+
 }//class end
-=======
-}//class end
->>>>>>> 8ed8dea8d71da38a270868c1ef7e003a724f82ac

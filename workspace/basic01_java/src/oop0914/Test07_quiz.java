0d0a1fb1e6c8178a03c177f259a84bda86f508ad
Daseul <mktmf1226@gmail.com>
2022-09-15 18:02:57 +0900

    220915 update

--- workspace/basic01_java/src/oop0914/Test07_quiz.java
+++ workspace/basic01_java/src/oop0914/Test07_quiz.java
@@ -8,13 +8,14 @@ public class Test07_quiz {
 		//문1)주민번호의 각 숫자의 합을 구하시오 (Character클래스와 charAt()함수 이용)
         //   8+9+1+2+3+0+1+2+3+4+5+6+7
 		
-		String jumin="8912301234567";
-		Character[] buno=new Character[jumin.length()];
+		String jumin="8912301234567";		
 		int hap=0;
 		
 		for(int i=0; i<jumin.length(); i++) {
-			buno[i]=jumin.charAt(i);
-			hap+=(int)(buno[i]-'0');
+			char buno=jumin.charAt(i);
+			//'8' -> 8
+			hap+=Character.getNumericValue(buno);
+			//hap+=(int)(buno-'0');
 		}//for end
 		System.out.println(hap); //51
 		
@@ -37,6 +38,16 @@ public class Test07_quiz {
         for(int i=0; i<size; i++) {
         	ch[i]=str.charAt(i);
         	
+        	if(Character.isUpperCase(ch[i])) {
+        		System.out.print(Character.toLowerCase(ch[i]));
+        	}else if(Character.isLowerCase(ch[i])) {
+        		System.out.print(Character.toUpperCase(ch[i]));
+        	}else {
+        		System.out.print(ch[i]);
+        	}//if end
+        	
+        	
+        	/*
 			if(ch[i]>='A' && ch[i]<='Z') {
 				System.out.printf("%c", ch[i]+32);
 			}//if end
@@ -44,6 +55,7 @@ public class Test07_quiz {
 			if(ch[i]>='a' && ch[i]<='z') {
 				System.out.printf("%c", ch[i]-32);
 			}//if end
+			*/
         }//for end
         
         
